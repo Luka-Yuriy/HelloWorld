8e6ac49c5d02483f53c5955067b647ee7fece743
Luka Yurii <lukayurii.work@gmail.com>
2023-05-27 18:21:20 +0500

    Setup UI

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -11,12 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        func fefee() {
-            
-        }
+        
+        
     }
 
 
 }
+
 
