41716bcade6680d2cd1465749c754e4d5e961cbc
Luka Yurii <lukayurii.work@gmail.com>
2023-05-27 18:55:44 +0500

    Working with logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,13 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLable: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greetingLable.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
         
     }
 
-
+    @IBAction func greetingButtonDidTapped() {
+        greetingLable.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLable.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal)
+    }
 }
 
 
